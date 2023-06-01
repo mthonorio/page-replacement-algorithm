f03541be95bc6c3ffd2605d63e77bd4e07a673df
Matheus Honorio <matheushonorio@eng.ci.ufpb.br>
2023-06-01 11:15:49 -0300

    fix: bug fixed index of list

--- otm.py
+++ otm.py
@@ -10,17 +10,22 @@ def optimal(file):
     frame = []  # Lista para armazenar as páginas na memória
 
     for page in pages:
+        distances = []
         if page not in frame:
             page_faults += 1
             if len(frame) == page_capacity:
                 # Encontra a página mais distante no futuro para substituir
                 future_pages = pages[pages.index(page):]
-                farthest_page = max(future_pages, key=future_pages.index)
-                frame[frame.index(farthest_page)] = page
+                index_of_farthest_page = max(future_pages, key=future_pages.index) - 1
+                farthest_page = future_pages[index_of_farthest_page]
+                if farthest_page not in frame:
+                    for f in frame:
+                        distance_of_f = (f, future_pages.index(f))
+                        distances.append(distance_of_f)
+                    max_distance = max(distances, key=lambda distance: distance[1])
+                    frame[frame.index(max_distance[0])] = farthest_page
             else:
                 frame.append(page)
-        else:
-            continue
 
     print(f"OTM {page_faults}")
     return page_faults
